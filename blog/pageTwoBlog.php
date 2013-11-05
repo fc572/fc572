18e7ccc56f511fdd67d66e4ab423da3595e00367
francesco <francesco@fc572.me>
2013-11-05 23:14:15 +0000

    Blog page two

---
+++ blog/pageTwoBlog.php
@@ -0,0 +1,42 @@
+<?php include "../templates/top.php"?>
+
+		<h2> Test your assumptions (and get lost!)</h2>
+			<p>
+		--------	Intro 		---------------------
+	I am reading "Decision" link and title and author and one of the chapters he writes about testing your assumpitions.
+	An assumption is something that we believe to be true (James definition from RST?) and that we think will hold true in light of
+	certain condition. But how long it takes for an assumpition to become an habit, something that you no longer know to be an assumption but 
+	you start to hold it as the "truth"? It does not takevery long, in my experience sometimes a matter of 2 weeks can lead you to remove 
+	the notion of that assumption from your head.
+	What is the risk in doing so? Well first and foremost if an assumption becomes normality we'll start to lean on it and the fall will be more painfull
+	Think of the times when you assume that the train will run on time when you have an important meeting first thing in the morning, only to get to 
+	the station and finding out that the train in front of you just broke down...
+		
+
+		--------	centre		--------------------
+	So what can we do to minimise the impact of our assumption? The book suggest that we test them from time to time; and how we do that? We test our
+	assumption by doing the very things that makes that assumption hold true. For example, my commute consist of riding a bike from point A to point B.
+	I assume, giving my knowledge of the area, my past experience and the map that I looked at before doing the first journey that route X1 is the quickest.
+	Time is something that I do not have the luxury of, expecially in the morning when I have to leave my son at nursery to then get to the office, so the 		quickest way for me its important. But from time to time, I do not run via route X1 but I choose to go via route X2 or X3. why I do that? I do it so that
+	I can test if my initial assumption are correct. It may be that route X2 is more longer but quicker, because has less traffic lights to go trough, or it 
+	has more cycleable ways that route X1. Or it may be that there is a short cut that it is is not shown by the maps, and you can only find out by riding the 
+	bike via that different route. Most of the time though testing your assumption will only cause you to waste time and effort and you'll end up getting to 
+	point B late, but in the process you'll have gained two important insight. First your initial assumption was correct, route X1 is quickest. Second you'll
+	have learned a new way to go from point A to point B that it might be useful in the future.And third, a different route means that your are zooming out 
+	from your usual routine and therefore tomorrow when you'll go trough the same most efficient old way, you'll look at it with differen eyes. 
+	
+
+		--------	conclusion  	--------------------
+	So even if sometimes testing your assumpitions means making a deliberate mistake, it is a necessary thing to do,to make sure that you are not 
+	fossilizzando on your way to be, but then you are choosing to do things that way because you want to and because there is a reaon to it, instaed
+ that falling in the habit trap. stay sharp by making some mistake here and there, in order to go out of the path that you have build yourself to check that that path
+	is still the most valid and relevant to you.
+
+		--------	summary 	--------------------
+		
+
+			</p>
+				<!--div class="linkButtonLeft"> <a href="../index.php"> Prev </a> </div--> 
+				<div class="linkButtonRight"> <a href="../c_sharp/pageOneCs.php"> Next </a> </div>
+		</div><!--centre-->
+<?php include "../templates/bottom.php"?>
