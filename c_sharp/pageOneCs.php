ae6f627e86a4899c5df1985c2a17b6508d1fee78
fc572 <francesco@fc572.me>
2013-11-14 22:26:58 +0000

    c sharp section

--- c_sharp/pageOneCs.php
+++ c_sharp/pageOneCs.php
@@ -1,41 +1,31 @@
-<?php include "../templates/top.php"?>
+﻿<?php include "../templates/top.php"?>
 
-		<h2> So here I am. Let's start with installing and running IDE of choice</h2>
+		<h2> Installing Visual Studio and WebDriver </h2>
 			<p>
-		Well I want to be able to do this in both c# and java, so I am going to do everything twice.<br/>
-		Let's start with C# for which we need to install <a href="http://www.microsoft.com/visualstudio/eng/downloads"> Visual studio Express </a>
-		where I choose Visual Studio Express 2012 for Web. Why this one? because it seems the most relevant since I am going to use selenium web driver.
-		There is the word web in both, I take this as a clue.<br/>
-		I have already installed this following the wizard, so no big surprises there. Now I am off to the selenium website in order
-		to find what I need to make selenium work with visual studio.<br/>
-		One of the things that I have found almost always together is that wheever I want C# and Selenium, NUnit most often than not shows 
-		in my searches. So what is it? As I understand it, Nunit is a framework for testing .net apps. What does that mean. If on google I do
-		"define: Framework", this is definition that I like best "A basic structure underlying a system". <br/> So NUnit allows me to use certain functionalities
-		like beign able to run a test without a Main() function and/or to report on those tests and to make the NUnit going trough my code and
-		finding all the tests that there are. I can then run them all together or one at the time. So I see NUnit as a very good plug in to Visual studio.
-		<br/>
-		So I am downloading NUnit from their <a href="http://www.nunit.org/index.php?p=download"> download page </a>
-		I am also going to download the C# driver from <a href "http://docs.seleniumhq.org/download/"> selenium download page </a>
-		<br/>
-		So now I have downloaded Selenium web driver "Language Client Version" C#, NUnit and Visual Studio 2012 Web
-		<br/>
-		I am opening Visual Studio and I click on New Project. I go to templates and then Visual C#. I am presented with is a dilemma; Which kind of project should I choose?
-		<br/>
-		The list is quite long, and that Test tab looks promising, but I do not want to run unit tests, I want something more functional, so I go for the 
-		empty web application. I name it mySeleniumDisaster and then I click OK. <br/>
-		Once the project is created, I know that I need to add NUnit and Web driver references to the project, so in solution explorer I click on the References
-		actually right click and I go on to add NUnit and webDriver dlls.<br/>
-		After adding the references I go and right click on seleniunDisaster and after choosing Add from the manu I select New item. I then go to 
-		code and click on class.<br/>
-		A skeleton class is open for me, but I need to do something with this. So I succumb to the dummest example, but I have to make sure that the set up works,
-		so I am going to search something on...bing! <br/>
-		So first things first. I have a class, now I need a method to make this class do something. But even before that, in order to user some of the feature
-		from NUnit, I have to use some magic words like [TestFixture][TestFixtureSetUp][Test] and [TestFixtureTearDown]
-		What are those? Test Fixture (in my head) is like saying to the machine "hey! This is going to be a test, so better run this code as it is", otherwise
-		the pc goes all clever and wants a main method somewhere. The TestFixtureSetUp, Test and TestFixtureTearDown are what is says on the tin. Set up, Test and 
-		destroy the test to free memory and kill all that is left hanging from this test before this machine mis-behave.<br/>
-		<textarea readonly rows=20 cols=95>
-		using System;
+<h5> The downloads </h5>
+Let's start with C# for which we need to install <a href="http://www.microsoft.com/visualstudio/eng/downloads"> Visual studio Express </a> where I choose Visual Studio Express 2012 for Web. 
+Why this one? because it seems the most relevant since I am going to use selenium web driver. There is the word web in both, I take this as a clue.<br/>
+I have already installed this following the wizard, so no big surprises there. Now I am off to the selenium website in order to find what I need to make selenium work with visual studio.<br/>
+
+One thing I noticed is that with C# and Selenium also NUnit most often than not shows in my searches. So what is it? As I understand it, Nunit is a framework for testing .net apps. What does that mean. If on google I do "define: Framework", I find this definition that I like: "A basic structure underlying a system". What is this structure and what allows me to do? The structure hides/takes care of complexity like beign able to run a program without a Main() function, to run and report all the tests that there are in my code and all I Have to do is to follow some simple rules. The tests can then run all together or one at the time. So I see NUnit as a very good thing to use alongside Visual studio.
+
+<br/>
+So I am downloading NUnit from their <a href="http://www.nunit.org/index.php?p=download"> download page </a> I am also going to download the C# driver from <a href "http://docs.seleniumhq.org/download/"> selenium download page </a>
+<br/>
+So now I have downloaded Selenium web driver "Language Client Version" C#, NUnit and Visual Studio 2012 Web, I am opening Visual Studio and I click on New Project. I go to templates and then Visual C#. I am presented with is a dilemma; Which kind of project should I choose?<br/>
+
+The list is quite long, and that Test tab looks promising, but I do not want to run unit tests, I want something more functional, so I go for the empty web application. I name it mySeleniumDisaster and then I click OK. <br/>
+<h5> The first simple project </h5>
+Once the project is created, I know that I need to add NUnit and Web driver references to the project, so in solution explorer I right click on the References and I go on to add NUnit and webDriver dlls.<br/>
+
+After adding the references I go and right click on seleniunDisaster and  choosing Add from the manu I select New item. I then go to code and click on class. A skeleton class is open for me, but I need to do something with this. So I succumb to the dummest example, but I have to make sure that the set up works,
+so I am going to search something on...bing! <br/>
+So first things first. I have a class and I now need a method to make this class do something. But before that, in order to user some of the feature from NUnit, I have to obey some rules and I have to use some magic words like [TestFixture][TestFixtureSetUp][Test] and [TestFixtureTearDown]
+What are those? Test Fixture (in my head) is like saying to the machine "hey! This is going to be a test, so better run this code as it is", otherwise
+the pc goes all clever and wants a main method somewhere. The TestFixtureSetUp, Test and TestFixtureTearDown are what is says on the tin. Set up, Test and 
+destroy the test to free memory and kill all that is left hanging from this test before this machine mis-behave.<br/>
+<textarea readonly rows=20 cols=95>
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -85,15 +75,11 @@ namespace mySeleniumDisaster
     }
 }</textarea><br/>
 
-		Another part that to me was quite obscure is the use of XPath and find element to search for text on the page. So I went and found out that 
-		<cite> // Selects nodes in the document from the current node that match the selection no matter where they are </cite> taken from <a href="http://www.w3schools.com/xpath/xpath_syntax.asp">here,
-		the nice guys at W3 School</a>
-		
-		I am debating with myself if explain everything that is on this page or not.... well it is 12:40am and I think it is time to sleep onit.
-		But we have success, and I have succesfully set up my project. Hurray!
-		</p>
-				<div class="linkButtonLeft"> <a href="misc.php"> Prev </a> </div> 
-				<div class="linkButtonRight"> <a href="pageTwoCs.php"> Next </a> </div>
-		</div><!--centre-->
+Another part that to me was quite obscure is the use of XPath and find element to search for text on the page. So I went and found out that <cite> Xpath Selects nodes in the document from the current node that match the selection no matter where they are </cite> taken from <a href="http://www.w3schools.com/xpath/xpath_syntax.asp">here, the nice guys at W3 School</a> I am debating with myself if explain everything that is on this page or not.... well it is 12:40am and I think it is time to sleep on it.
+And I go to bed happy because the project compiles and runs succesfully. Hurray!
+</p>
+<div class="linkButtonLeft"> <a href="misc.php"> Prev </a> </div> 
+<div class="linkButtonRight"> <a href="pageTwoCs.php"> Next </a> </div>
+</div><!--centre-->
 		
 <?php include "../templates/bottom.php"?>
