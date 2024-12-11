32e95acd037b92512533659c0b55506792254660
kekko <fc572@kekkos-MacBook-Air.local>
2024-12-11 09:53:10 +0000

    frontend fix

--- frontend/js/funCall.js
+++ frontend/js/funCall.js
@@ -1,24 +1,18 @@
 export async function callLambda(httpCode) {
-    const lambdaUrl = "LAMBDA_URL_HTML";
-    const lambdaApiKey = "LAMBDA_API_KEY_HTML";
-    const apiUrl = `${lambdaUrl}/httpcodes/${httpCode}`;
+   const proxyUrl = `/proxy/httpcodes/${httpCode}`;
+   try {
+       const response = await fetch(proxyUrl, {
+           method: 'GET',
+       });
 
-    try {
-        const response = await fetch(apiUrl, {
-            method: 'GET',
-            headers: {
-                'x-api-key': lambdaApiKey
-            }
-        });
-
-        if (response.ok) {
-            const data = await response.text();
-            alert(`Response from Lambda: ${data}`);
-        } else {
-            alert(`Error: ${response.status} ${response.statusText}`);
-        }
-    } catch (error) {
-        alert(`Failed to call Lambda function: ${error.message}`);
-    }
+       if (response.ok) {
+           const data = await response.text();
+           alert(`Response from Lambda: ${data}`);
+       } else {
+           alert(`Error: ${response.status} ${response.statusText}`);
+       }
+   } catch (error) {
+       alert(`Failed to call Lambda function: ${error.message}`);
+   }
 }
 window.callLambda = callLambda;
